a58b44f13fb9247d300db0ce23ec607fa18869b0
Bewvy Worapat <nokhook_bew@hotmail.com>
2020-05-24 22:47:39 +0700

    add sudo

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-apt-get update
-apt upgrade
+sudo apt-get update
+sudo apt upgrade
 
 #install zsh
-apt install zsh
+sudo apt install zsh
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-chsh -s $(which zsh) $(whoami)
+sudo chsh -s $(which zsh) $(whoami)
 
-apt install fonts-powerline
+sudo apt install fonts-powerline
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1

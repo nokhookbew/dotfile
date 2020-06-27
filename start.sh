e7f94fc667df5e2182c79fdc52ad0047939e1767
Bewvy Worapat <nokhook_bew@hotmail.com>
2020-06-27 17:27:22 +0700

    add power10k

--- start.sh
+++ start.sh
@@ -14,7 +14,10 @@ sudo apt install fonts-powerline
 
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+
+sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/dotfile/.zshrc
 
 mv .tmux.conf ..
 mv .vimrc ..
